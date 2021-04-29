0990f068f0c471bbf5d9a6047ce04b80493de3bb
Catarina Castro <cmsacastro@gmail.com>
2021-04-29 21:44:13 +0100

    watchlist

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,25 @@
+class BookmarksController < ApplicationController
+
+  def index
+    @bookmarks = Bookmark.all
+  end
+
+  def new
+    @list = List.find(params[:list_id])
+    @bookmark = Bookmark.new
+  end
+
+  def create
+    @bookmark = Bookmark.new(bookmark_params)
+    @list = List.find(params[:list_id])
+    @bookmark.list = @list
+    @bookmark.save
+    redirect_to list_path(@list)
+  end
+
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment, :movie_id)
+  end
+end
---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,29 @@
+class ListsController < ApplicationController
+
+  def index
+    @lists = List.all
+  end
+
+  def show
+    @list = List.find(params[:id])
+    @movies = @list.movies
+    @bookmarks = @list.bookmarks
+  end
+
+  def new
+    @list = List.new
+  end
+
+  def create
+    @list = List.new(list_params)
+    @list.save
+    redirect_to lists_path
+  end
+
+
+  private
+
+  def list_params
+    params.require(:list).permit(:name)
+  end
+end
