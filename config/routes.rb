a0f7ab2a30d5faa324a1af14d49d2c3c072e7895
Catarina Castro <cmsacastro@gmail.com>
2021-05-03 16:54:05 +0100

    css added

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :lists do
-    resources :bookmarks, only: [:index, :show, :new, :create, :destroy]
+    resources :bookmarks, only: [:index, :show, :new, :create]
   end
+  resources :bookmarks, only: :destroy
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
