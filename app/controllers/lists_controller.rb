e01ed006d39e9bc7ed9e3b2831bb18416144f073
Catarina Castro <cmsacastro@gmail.com>
2021-05-04 20:36:49 +0100

    Add dotenv - Protect my secret data in .env file

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -30,6 +30,6 @@ class ListsController < ApplicationController
   private
 
   def list_params
-    params.require(:list).permit(:name)
+    params.require(:list).permit(:name, :image)
   end
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,6 +1,6 @@
 class Movie < ApplicationRecord
   has_many :bookmarks
-  # has_one_attached :image, dependent: :destroy
+  has_one_attached :image, dependent: :destroy
   validates :title, uniqueness: true
   validates :title, presence: true
   validates :overview, presence: true
